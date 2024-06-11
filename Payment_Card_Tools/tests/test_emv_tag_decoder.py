179e05e9e02c2642197eacbf164f756bb82a3de3
Thanigaivel S G <thanigaigopal@outlook.com>
2024-06-11 22:03:12 +0530

    Updated with TVR Byte Validation

--- Payment_Card_Tools/tests/test_emv_tag_decoder.py
+++ Payment_Card_Tools/tests/test_emv_tag_decoder.py
@@ -10,6 +10,7 @@ class Test_EMV_Tag_Decoder():
     input_tvr_loc = (By.XPATH, "//input[@id='input-tvr']")
     btn_tvr_loc = (By.CSS_SELECTOR, "#button-tvr-decode")
     div_tvr_decoded_loc = (By.CSS_SELECTOR, "#tvr-decoded")
+    flat_tvr_loc = (By.ID, "input-tvr-flat")
     
     def test_expand_emv_tag_decoder(self):
         emv_tags = [ele.text for ele in self.driver.find_elements(*Test_EMV_Tag_Decoder.div_emv_tag_loc)]
@@ -28,4 +29,14 @@ class Test_EMV_Tag_Decoder():
         if not any(tvr_decoded.find_elements(By.CSS_SELECTOR, "*")):
             self.driver.find_element(*Test_EMV_Tag_Decoder.btn_tvr_loc).click()
 
-        time.sleep(5)
+        assert any(tvr_decoded.find_elements(By.CSS_SELECTOR, "*"))
+
+        self.driver.find_element(*Test_EMV_Tag_Decoder.flat_tvr_loc).click()
+
+        tvr_byte_1_bit_8 = tvr_decoded.find_element(By.XPATH, "//div/div[1]/table/tbody/tr[2]/td[1]/span").text
+
+        assert tvr_byte_1_bit_8 == "2", "TVR Byte 1 Bit 8 is set to 1"
+
+        tvr_byte_1_bit_8_highlighted = tvr_decoded.find_element(By.XPATH, "//div/div[1]/table/tbody/tr[2]/td[9]/span").get_attribute('class')
+
+        assert tvr_byte_1_bit_8_highlighted == "bgset", "Offline data authentication was not performed"
